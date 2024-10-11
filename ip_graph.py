75086cbc117464572a7c986861208915267cf75a
ningjx <hapyning@outlook.com>
2024-10-11 14:26:09 +0800

    1

--- ip_graph.py
+++ ip_graph.py
@@ -10,24 +10,31 @@ class IPGraph:
         self.y_offset = y_offset
         self.font_large = ImageFont.truetype(font_path, 14)
         self.bitmap_Tmp = bitmaps.get_bitmap_IP()
-
+        self.buffer_image = Image.new('1', (width, height))
+        self.buffer_draw = ImageDraw.Draw(self.buffer_image)
         self.scroll_offset = 0  # 初始偏移量
         self.scroll_speed = 1  # 滚动速度
-
+        self.pre_ip = "ip"
+        self.static_offset = 0
+        self.direction = False
 
     def draw(self, ip):
         bitmaps.draw_bitmap(self.draw_obj, self.bitmap_Tmp, self.x_offset, self.y_offset)
-        buffer_image = Image.new('1', (self.width, self.height))
-        buffer_draw = ImageDraw.Draw(buffer_image)
+        self.buffer_draw.rectangle((0,0,self.width,self.height),fill=0)
+        if ip != self.pre_ip:
+            self.pre_ip = ip
+            max_usage_box = self.draw_obj.textbbox((0, 0), ip, font=self.font_large)
+            text_width = max_usage_box[2]
+            self.static_offset = text_width - (128 - (self.x_offset + 14))
+        
+        if self.static_offset > 0:
+            if self.static_offset < - self.scroll_offset or self.scroll_offset > 0:
+                self.direction = not self.direction
+            if self.direction:
+                self.scroll_offset -= self.scroll_speed
+            else:
+                self.scroll_offset += self.scroll_speed
 
-        max_usage_box = self.draw_obj.textbbox((0, 0), ip, font=self.font_large)
-        text_width = max_usage_box[2]
-        # 如果文本宽度超出屏幕宽度，开始滚动
-        if text_width > 128 - (self.x_offset + 14):
-            # 更新滚动偏移量
-            self.scroll_offset -= self.scroll_speed
-            if self.scroll_offset <  128 - (self.x_offset + 14) - text_width:  # 如果偏移量超过文本宽度，则重置
-                self.scroll_offset = 0  # 从右侧重新开始滚动
-        #self.draw_obj.text((self.x_offset + 14, self.y_offset - 5), ip, fill=1, font=self.font_large)
-        buffer_draw.text((self.scroll_offset, - 5), ip, fill=1, font=self.font_large)
-        self.draw_obj.bitmap((self.x_offset + 14, self.y_offset), buffer_image, fill=1)
+        self.buffer_draw.text((self.scroll_offset, - 5), ip, fill=1, font=self.font_large)
+
+        self.draw_obj.bitmap((self.x_offset + 14, self.y_offset), self.buffer_image, fill=1)
